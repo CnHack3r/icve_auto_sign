7c215ed8f6926424e145815905f0f8b66945ca46
CnHack3r <96738114+CnHack3r@users.noreply.github.com>
2021-12-30 02:03:58 +0800

    Add files via upload

---
+++ 服务器端程序/Lib/site-packages/dbutils/__init__.py
@@ -0,0 +1,8 @@
+# DBUtils main package
+
+__all__ = [
+    '__version__',
+    'simple_pooled_pg', 'steady_pg', 'pooled_pg', 'persistent_pg',
+    'simple_pooled_db', 'steady_db', 'pooled_db', 'persistent_db']
+
+__version__ = '2.0'
